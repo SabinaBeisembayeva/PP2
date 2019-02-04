e0368aecafd621c2fa027743c1da2d34e1021263
Sabina <sabinakassenova@gmail.com>
2019-02-04 23:32:56 +0600

    comment

---
+++ Week1/Task4/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //create integer - number of lines
+            for(int i=0; i<n; i++) //number of rows
+            {
+                for(int j=0; j<=i+1; j++) //number of columns
+                {
+                    Console.Write("[*]"); // command to see on a screen needed representation =
+                }
+                Console.WriteLine(); // switches to the next line 
+            }
+            Console.ReadKey(); // terminal will not close immediatly 
+        }
+    }
+}
