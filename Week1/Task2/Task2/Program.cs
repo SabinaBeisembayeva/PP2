afe09611bbdcf569a3065da1db230c1c630c2ec5
Sabina <sabinakassenova@gmail.com>
2019-02-05 00:49:13 +0600

    yey

---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,58 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year = 1;
+
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+
+
+        public void print()
+
+        {
+            year++;
+            Console.WriteLine(name + " " + id + " " + year);
+
+
+
+        }
+        
+
+    }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Student s = new Student("Sabina", "18BD110244");
+                s.print();
+
+            Console.ReadKey();   
+
+
+        }
+        
+
+           
+
+        }
+   
+    
+
+       
+    }
+
