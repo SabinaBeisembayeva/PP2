234abc74601cdc592d881733fea3a18f06c29af3
Sabina <sabinakassenova@gmail.com>
2019-02-12 04:26:48 +0600

    tadamtsss

---
+++ Week2/Task3/Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        public static void PrintSpaces(int a)
+        {
+            for(int i=0;i<a; i++)
+            {
+                Console.Write(" ");
+            }
+        
+        }
+        public static void Directory(DirectoryInfo direct, int n)
+        {
+            FileInfo[] files = direct.GetFiles();
+            DirectoryInfo[] directories = direct.GetDirectories();
+
+            foreach(FileInfo file in files)
+            {
+                PrintSpaces(n);
+                Console.WriteLine(file.Name);
+            }
+
+            foreach(DirectoryInfo dir in directories)
+            {
+                PrintSpaces(n);
+                Console.WriteLine(dir.Name);
+                Directory(dir, n + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Sabina\pp2\PP2");
+            Directory(dir, 1);
+            Console.ReadKey();
+
+        }
+    }
+}
