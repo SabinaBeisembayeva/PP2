5b721b98ec3a636e4682fd03ddc08825e9262d3a
Sabina <sabinakassenova@gmail.com>
2019-02-12 03:38:25 +0600

    CORRECT

--- Week2/Task2/Task2/Program.cs
+++ Week2/Task2/Task2/Program.cs
@@ -9,15 +9,15 @@ namespace Task2
 {
     class Program
     {
-        public static bool IsPrime(int n)
+        public static bool IsPrime(int n) //creating a function to check the prime 
         {
-            if (n == 1) return false;
-            if (n == 0) return false;
+            if (n == 1) return false; // if the number is 1, it is not a prime
+            if (n == 0) return false; // similarly with 0
 
             
-            for(int i=2; i<n; i++)
+            for(int i=2; i<n; i++) // entering a cycle , starting from the smallest prime number = 2
             {
-                if (n % i == 0) return false;
+                if (n % i == 0) return false; 
             }
             return true; 
 
@@ -26,20 +26,20 @@ namespace Task2
 
        
 
-     static void Main(string[] args)
+     static void Main(string[] args) // main func
         {
-            StreamReader sr = new StreamReader(@"C:\Users\Sabina\pp2\PP2\Week2\Task2\Task2\input.txt");
-            string[] s = sr.ReadToEnd().Split();
-            StreamWriter sw = new StreamWriter(@"C:\Users\Sabina\pp2\PP2\Week2\Task2\Task2\output.txt");
+            StreamReader sr = new StreamReader(@"C:\Users\Sabina\pp2\PP2\Week2\Task2\Task2\input.txt"); // streamreader reads text from txt files
+            string[] s = sr.ReadToEnd().Split(); // creating the string and read all elements in the txt to the end and split them into substrings
+            StreamWriter sw = new StreamWriter(@"C:\Users\Sabina\pp2\PP2\Week2\Task2\Task2\output.txt"); // streamwriter writes elements into a text files
             for(int i=0; i<s.Length; i++)
             {
                 if (IsPrime(int.Parse(s[i])))
                 {
-                    sw.Write(s[i] + " ");
+                    sw.Write(s[i] + " "); // if the numbers are prime, vyvodym them in output txt file 
                 }
                 
             }
-            sw.Close();
+            sw.Close(); // closes StreamReader 
 
 
 
