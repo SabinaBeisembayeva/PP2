10a9e13a21b83a39a1a9494ac755b56af393ba46
Sabina <sabinakassenova@gmail.com>
2019-02-12 22:20:08 +0600

    first commit

---
+++ Week3/Task1/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Sabina\Desktop\PP2");
+            DirectoryInfo[] directories = dir.GetDirectories();
+            for (int i = 0; i < directories.Length; i ++)
+            {
+               
+                if(i==0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    
+                }
+
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                }
+
+                DirectoryInfo dir2 = directories[i];
+                Console.WriteLine(dir2.Name);
+
+
+
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
