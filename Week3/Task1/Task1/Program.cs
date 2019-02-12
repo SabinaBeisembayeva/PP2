4ca07a7be2795d1027be907755be4a3aaffad26e
Sabina <sabinakassenova@gmail.com>
2019-02-12 23:07:52 +0600

    Second try

--- Week3/Task1/Task1/Program.cs
+++ Week3/Task1/Task1/Program.cs
@@ -9,21 +9,18 @@ namespace Task1
 {
     class Program
     {
-        
 
-        static void Main(string[] args)
+        public static void far(DirectoryInfo dir, int pos)
         {
-            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Sabina\Desktop\PP2");
+            Console.Clear();
             DirectoryInfo[] directories = dir.GetDirectories();
-            for (int i = 0; i < directories.Length; i ++)
+            FileInfo[] files = dir.GetFiles();
+            for (int i = 0; i < directories.Length; i++)
             {
-               
-                if(i==0)
+                if (i == pos)
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
-                    
                 }
-
                 else
                 {
                     Console.BackgroundColor = ConsoleColor.Green;
@@ -31,13 +28,46 @@ namespace Task1
 
                 DirectoryInfo dir2 = directories[i];
                 Console.WriteLine(dir2.Name);
+            }
 
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+            if(consoleKey.Key == ConsoleKey.UpArrow)
+            {
+                if(pos == 0)
+                {
+                    far(dir, 0);
+                }
+                else 
+                {
+                    far(dir, pos-1);
+                }
+                
+                
+            }
 
-
+            if(consoleKey.Key == ConsoleKey.DownArrow)
+            {
+                if(pos == directories.Length - 1)
+                {
+                    far(dir, directories.Length - 1); 
+                }
+                else
+                {
+                    far(dir, pos + 1);
+                }
+                 
 
             }
 
+        }
+        
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Sabina\Desktop\PP2");
+            far(dir, 3);
             Console.ReadKey();
+
 
 
         }
