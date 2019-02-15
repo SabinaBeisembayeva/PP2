840ed83778a9806afea77e28f5fe005a30ce9339
Sabina <sabinakassenova@gmail.com>
2019-02-15 12:46:25 +0600

    let cats live forever

---
+++ Week2/Task4/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string putin = @"C:\Users\Sabina\pp2\PP2\Week2\Task4\putin";
+            string russia = "RH";
+
+            string from = Path.Combine(putin, russia);
+
+            FileStream fs = File.Create(from);
+            fs.Close(); 
+
+            string nazarbayev = @"C:\Users\Sabina\pp2\PP2\Week2\Task4\nazarbayev";
+            string to = Path.Combine(nazarbayev, russia);
+
+            if (File.Exists(from))
+            {
+                File.Delete(to);  
+            }
+
+            File.Copy(from, to);
+
+            File.Delete(from);
+
+            
+
+
+           
+
+
+
+        }
+    }
+}
